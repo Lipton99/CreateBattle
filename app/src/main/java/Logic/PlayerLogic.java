6c4d2f025e34521dbd87e33a244fdf145ea6883e
Lipton99 <obake.calibur09@gmail.com>
2016-11-01 14:08:32 +0900

    add Logic/PlayerLogic

---
+++ app/src/main/java/Logic/PlayerLogic.java
@@ -0,0 +1,18 @@
+package jp.radiocat.example.ormExample.activity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.*;
+import jp.radiocat.example.ormExample.R;
+import jp.radiocat.example.ormExample.entity.Word;
+import jp.radiocat.example.ormExample.model.WordModel;
+
+public class PlayerLogic {
+  //Playerに関する情報整形
+}
