ff1481d373de3fa2ca2c0ad97243efe53f00d482
Lipton99 <obake.calibur09@gmail.com>
2016-11-01 14:33:55 +0900

    Logic method change

--- app/src/main/java/com/example/tsubasa/createbattle/CameraActivity.java
+++ app/src/main/java/com/example/tsubasa/createbattle/CameraActivity.java
@@ -22,11 +22,14 @@ import java.util.List;
 
 import Const.CommonConst;
 import Model.Player;
+import Logic.PlayerLogic;
 
 public class CameraActivity extends AppCompatActivity {
 
+    //カメラ取得画像設定View
     ImageView imageView;
-    final int MAX_FACES = 4;
+    //顔情報
+    HashMap<String, String> faceStatusData = new HashMap<String, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,69 +66,18 @@ public class CameraActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CommonConst.RESULT_CAMERA) {
-            //顔情報
-            HashMap<String, String> faceStatusData = new HashMap<String, String>();
-            //顔情報種別
-            int faceType = 0;
-
-            //カメラで取得した画像のとりこみ
+            //カメラで取得した画像取得
             Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-
-            //顔認識クラス宣言
-            FaceDetector detector = new FaceDetector.Builder(getApplicationContext())
-                    .setTrackingEnabled(false)
-                    .setLandmarkType(FaceDetector.ALL_LANDMARKS)
-                    .build();
-
-            //カメラ画像に対して顔情報取得
-            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
-            SparseArray<Face> faces = detector.detect(frame);
-
-            //顔数を取得
-            int faceCount = faces.size();
-            //顔認識された場合、
-            if (faceCount > 0) {
-                for (int i = 0; i < faces.size(); ++i) {
-                    Face face = faces.valueAt(i);
-                    //顔の詳細情報の取得
-                    for (Landmark landmark : face.getLandmarks()) {
-                        //顔情報種別の取得
-                        faceType = landmark.getType();
-                        //顔種別別にステータスを割り振る
-                        switch (faceType) {
-                            //口の位置
-                            case Landmark.BOTTOM_MOUTH:
-                                faceStatusData.put(String.valueOf(Landmark.BOTTOM_MOUTH), String.valueOf(landmark.getPosition()));
-                                Log.d("CreateBattle", "CameraActivity BOTTOM_MOUTH = " + landmark.getPosition());
-                                break;
-                            //鼻の位置
-                            case Landmark.NOSE_BASE:
-                                faceStatusData.put(String.valueOf(Landmark.NOSE_BASE), String.valueOf(landmark.getPosition()));
-                                Log.d("CreateBattle", "CameraActivity NOSE_BASE = " + landmark.getPosition());
-                                break;
-                            //左目の位置
-                            case Landmark.LEFT_EYE:
-                                faceStatusData.put(String.valueOf(Landmark.LEFT_EYE), String.valueOf(landmark.getPosition()));
-                                Log.d("CreateBattle", "CameraActivity LEFT_EYE = " + landmark.getPosition());
-                                break;
-                            //右目の位置
-                            case Landmark.RIGHT_EYE:
-                                faceStatusData.put(String.valueOf(Landmark.RIGHT_EYE), String.valueOf(landmark.getPosition()));
-                                Log.d("CreateBattle", "CameraActivity RIGHT_EYE = " + landmark.getPosition());
-                                break;
-                        }
-                    }
-                }
-                //画面にカメラ画像を設定する
+            //カメラ画像あら顔情報取得
+            faceStatusData = PlayerLogic.getFaceStatus(bitmap);
+            //顔情報がある場合、Viewに設定する
+            if(!empty(faceStatusData)){
                 imageView.setImageBitmap(bitmap);
-            } else {
-                //TODO:顔が取れていない場合は再度取得を促す
             }
         }
     }
