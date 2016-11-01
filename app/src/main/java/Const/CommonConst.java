8572ba0d99fdd2266eff6a6383141b1ea1bb591f
Lipton99 <obake.calibur09@gmail.com>
2016-11-01 14:55:29 +0900

    add Const FaceBitMapFilePath

--- app/src/main/java/Const/CommonConst.java
+++ app/src/main/java/Const/CommonConst.java
@@ -11,5 +11,7 @@ public class CommonConst {
     public final static int RESULT_PLAYER_LIST_ACTIVITY = 3000;
     //バトル画面遷移結果
     public final static int RESULT_BATTLE_ACTIVITY = 4000;
+    //顔画像保存先ディレクトリ
+    public final static String CommonConst.FACE_BITMAP_FILE_PATH = "./data/faceBitmap/";
 
 }
