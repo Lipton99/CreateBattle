14078a9f405a73a2cf4fbd807e664c20f89c982c
Lipton99 <obake.calibur09@gmail.com>
2016-11-16 20:12:56 +0900

    ADD call createPlayerDataMockMethod

--- app/src/main/java/com/example/tsubasa/createbattle/BattleActivity.java
+++ app/src/main/java/com/example/tsubasa/createbattle/BattleActivity.java
@@ -4,11 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class BattleActivity extends AppCompatActivity {
+    
+    //プレイヤー1情報
+    HashMap<String, String> playerData1 = new HashMap<String, String>();
+    //プレイヤー2情報
+    HashMap<String, String> playerData2 = new HashMap<String, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle);
-        //comitテスト
+        
+        //TODO:mockPlayerData@本来はDBからPlayerIDで取得する
+        Player player1 = TestMockLogic.createPlayerDataMock("モック1",9999);
+        Player player2 = TestMockLogic.createPlayerDataMock("モック2",9998);
+        
     }
 }
